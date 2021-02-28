eab3b586103507bd56c50472cf118fba85ca10d7
Joe Wolfe <joe.wolfe21@gmail.com>
2021-02-28 11:43:22 -0500

    remove unique constraint

--- api/models.py
+++ api/models.py
@@ -7,8 +7,8 @@ class User(db.Model):
     __tablename__ = "users"
 
     id = db.Column(db.Integer, primary_key=True)
-    first_name = db.Column(db.String(80), unique=True, nullable=False)
-    last_name = db.Column(db.String(80), unique=True, nullable=False)
+    first_name = db.Column(db.String(80), nullable=False)
+    last_name = db.Column(db.String(80), nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     username = db.Column(db.String(80), unique=True, nullable=False)
     password = db.Column(db.String(100))
