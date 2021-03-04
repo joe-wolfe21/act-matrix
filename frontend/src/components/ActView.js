504d9512982125ea97d183b648e8b829cc909410
Joe Wolfe <joe.wolfe21@gmail.com>
2021-03-04 14:28:36 -0500

    improved text field

--- frontend/src/components/ActView.js
+++ frontend/src/components/ActView.js
@@ -1,9 +1,18 @@
 import { useState } from "react";
 import Button from "@material-ui/core/Button";
+import TextField from "@material-ui/core/TextField";
 
 import ActQuestion from "./ActQuestion";
 import ResultsView from "./ResultsView";
 import questions from "../questions";
+
+const styles = {
+  helper: {
+    color: "blue",
+    textAlign: "right",
+    margin: "0",
+  },
+};
 
 const ActView = () => {
   const [currentQuestion, setCurrentQuestion] = useState(0);
@@ -41,11 +50,20 @@ const ActView = () => {
   return (
     <div className="act-view">
       <ActQuestion questionImg={questionImg} />
-      <textarea
+
+      <TextField
         className="act-answer"
-        placeholder="starting writing..."
+        id="outlined-multiline-static"
+        label="Answer"
+        multiline
+        rows={4}
+        placeholder="start writing..."
         value={answer}
+        variant="outlined"
         onChange={handleUpdateAnswer}
+        helperText={answer.length + "/140"}
+        inputProps={{ maxLength: 140 }}
+        FormHelperTextProps={{ style: styles.helper }}
       />
       <div className="button-group">
         {currentQuestion > 0 && (
