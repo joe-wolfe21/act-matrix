39347178bfd2a2fd68baa6023d01724a5b497612
Joe Wolfe <joe.wolfe21@gmail.com>
2021-03-05 15:57:20 -0500

    post a matrix

--- backend/api/routes.py
+++ backend/api/routes.py
@@ -1,8 +1,12 @@
 from flask import current_app as app
 from flask import jsonify
+from flask import request
 
+from .models import db
+from .models import Matrix
 from .models import User
 from .schemas import matrices_schema
+from .schemas import matrix_schema
 from .schemas import user_schema
 from .schemas import users_schema
 
@@ -40,3 +44,28 @@ def user_matrix_records(user_id):
     user = User.query.get(user_id)
 
     return {"matrices": matrices_schema.dump(user.matrices)}
+
+
+@app.route("/api/users/<uuid:user_id>/matrices", methods=["POST"])
+def user_add_matrix_record(user_id):
+    """Get the matrices for user with `user_id`.
+    """
+    User.query.get(user_id)
+
+    answer_bottom_right = request.json.get("answer_bottom_right", "")
+    answer_bottom_left = request.json.get("answer_bottom_left", "")
+    answer_top_left = request.json.get("answer_top_left", "")
+    answer_top_left = request.json.get("answer_top_left", "")
+
+    matrix = Matrix(
+        user_id=user_id,
+        answer_bottom_right=answer_bottom_right,
+        answer_bottom_left=answer_bottom_left,
+        answer_top_left=answer_top_left,
+        answer_top_right=answer_bottom_right,
+    )
+
+    db.session.add(matrix)
+    db.session.commit()
+
+    return {"matrix": matrix_schema.dump(matrix)}
