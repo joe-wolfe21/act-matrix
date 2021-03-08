2111ee384e395389964313982cddfb28abec4998
Joe Wolfe <joe.wolfe21@gmail.com>
2021-03-07 19:51:40 -0500

    wip

--- frontend/src/components/MatrixHistory.js
+++ frontend/src/components/MatrixHistory.js
@@ -1,5 +1,8 @@
 import React from "react";
 
+// use accordion to display my most recent
+// when clicked, drop down with the matrix
+
 const MatrixHome = () => <h2>Matrices</h2>;
 
 export default MatrixHome;
