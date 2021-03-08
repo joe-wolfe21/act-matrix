4cdf41099241a41a0b52b1daf71bac643503ff7d
Joe Wolfe <joe.wolfe21@gmail.com>
2021-03-08 08:51:35 -0500

    create accordion view of past matrix submissions

--- frontend/src/components/MatrixHistory.js
+++ frontend/src/components/MatrixHistory.js
@@ -1,8 +1,66 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Accordion from "@material-ui/core/Accordion";
+import AccordionSummary from "@material-ui/core/AccordionSummary";
+import AccordionDetails from "@material-ui/core/AccordionDetails";
+import Typography from "@material-ui/core/Typography";
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
 
-// use accordion to display my most recent
-// when clicked, drop down with the matrix
+const useStyles = makeStyles((theme) => ({
+  root: {
+    width: "100%",
+  },
+  heading: {
+    fontSize: theme.typography.pxToRem(15),
+    fontWeight: theme.typography.fontWeightRegular,
+  },
+}));
 
-const MatrixHome = () => <h2>Matrices</h2>;
+const MatrixHistory = () => {
+  const [completedMatrices, setCompletedMatrices] = useState([]);
+  const classes = useStyles();
 
-export default MatrixHome;
+  // replace
+  const url = "/api/users/4b0caca0-eed4-4156-bb57-28200d0eeea8/matrices";
+  useEffect(() => {
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => setCompletedMatrices(data.matrices));
+  }, [url]);
+
+  console.log(completedMatrices);
+
+  return (
+    <div className={classes.root}>
+      {completedMatrices.map((completedMatrix) => (
+        <CompletedMatrix
+          key={completedMatrix.id}
+          createdOn={completedMatrix.created_on}
+        />
+      ))}
+    </div>
+  );
+};
+
+const CompletedMatrix = ({ createdOn }) => {
+  const classes = useStyles();
+
+  return (
+    <Accordion>
+      <AccordionSummary
+        expandIcon={<ExpandMoreIcon />}
+        aria-controls="panel1a-content"
+        id="panel1a-header"
+      >
+        <Typography className={classes.heading}>{createdOn}</Typography>
+      </AccordionSummary>
+      <AccordionDetails>
+        <Typography>
+          Replace me with an Act Matrix pulled from ResultsView
+        </Typography>
+      </AccordionDetails>
+    </Accordion>
+  );
+};
+
+export default MatrixHistory;
